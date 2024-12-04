3a18596f3aaf65d77c901eadd12ce2b7deb664e6
Parker Kain <parker.kain@8451.com>
2024-12-04 07:53:30 -0500

    unused code

--- 2024/python/day_4/pt_2.py
+++ 2024/python/day_4/pt_2.py
@@ -3,29 +3,7 @@ with open("input") as lines:
     for line in lines:
         input.append(line.replace("\n", ""))
 
-# print(input)
-
 times_found = 0
-
-
-def search_adjacent(center: tuple[int, int], needle: str):
-    matches = []
-    for x_delta in [-1, 0, 1]:
-        for y_delta in [-1, 0, 1]:
-            if (0 > center[0] + 3 * x_delta) or (center[0] + 3 * x_delta >= len(input)):
-                print("Skipping...")
-                continue
-            if (0 > center[1] + 3 * y_delta) or (center[1] + 3 * y_delta >= len(input)):
-                print("Skipping...")
-                continue
-            print(center[0] + x_delta, center[1] + y_delta)
-            curr = center[0] + x_delta, center[1] + y_delta
-            if input[curr[0]][curr[1]] == needle:
-                print(f"Adjacent {needle} found at {curr}")
-                matches.append((curr, (x_delta, y_delta)))
-
-    return matches
-
 
 # Look for A's, then look for the others
 for i in range(1, len(input) - 1):
